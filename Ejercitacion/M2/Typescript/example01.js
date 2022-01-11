54235c841b68c8c300f0ec83b0de045ec9fa14d4
unknown <fragusan2@gmail.com>
2022-01-11 10:07:04 -0300

    add exercices typescript & boilerplate  PF

---
+++ Ejercitacion/M2/Typescript/example01.js
@@ -0,0 +1,12 @@
+/* let a: number; // explicita
+let b: string; // explicita
+let c=101; //implícita
+c="one";
+
+//═════════════════════════════════════════════════════
+
+let recursos= ['memoria', 'disco','procesador', 100]
+
+let recurso : string [] = ['memoria', 'disco','procesador'] */
+var message = "Programando con Typescript";
+console.log(message);
---
+++ Ejercitacion/M2/Typescript/example01.ts
@@ -0,0 +1,13 @@
+/* let a: number; // explicita
+let b: string; // explicita
+let c=101; //implícita
+c="one";
+
+//═════════════════════════════════════════════════════
+
+let recursos= ['memoria', 'disco','procesador', 100]
+
+let recurso : string [] = ['memoria', 'disco','procesador'] */
+
+let message: string = "Programando con Typescript";
+console.log(message);
